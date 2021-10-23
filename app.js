2f5f9d53d2822f5bb4d5407f0830f39c3e42b096
dreadlocker <svetozariliev13@gmail.com>
2021-10-23 11:05:59 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -50,9 +50,9 @@ app.use('/favicon.ico', express.static(path.join(__dirname, 'favicon.ico')));
 app.use(cors({
   // // for local test
   // "origin": "http://localhost:8080",
-  // "origin": "*",
+  "origin": "*",
   // // FOR PRODUCTION
-  "origin": "https://bqla.github.io/tehnika/",
+  // "origin": ["https://bqla.github.io/", "https://bqla.github.io/tehnika"],
   "methods": "GET,PUT,POST,DELETE",
   "preflightContinue": false,
 }));
